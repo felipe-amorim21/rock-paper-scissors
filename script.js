04a8d11e366356da0a266bd41e42c1c7fb23613a
Felipe Amorim <felipe21fab@gmail.com>
2022-06-14 20:24:17 -0300

    Implemented playRound function

--- script.js
+++ script.js
@@ -15,3 +15,39 @@ function computerPlay() {
   return computerPick
 }
 
+function playRound(playerSelection, computerSelection) {
+  playerSelection = playerSelection.toUpperCase()
+  switch (playerSelection) {
+    case 'ROCK':
+      if (computerSelection === 'Rock') {
+        return "It's a DRAW"
+      } else if (computerSelection === 'Paper') {
+        computer++
+        return 'You LOSE! Paper beats Rock'
+      } else {
+        player++
+        return 'You WIN! Rock beats Scissors'
+      }
+    case 'PAPER':
+      if (computerSelection === 'Paper') {
+        return "It's a DRAW"
+      } else if (computerSelection === 'Rock') {
+        computer++
+        return 'You LOSE! Scissors beats Paper'
+      } else {
+        player++
+        return 'You WIN! Paper beats Rock'
+      }
+    case 'SCISSORS':
+      if (computerSelection === 'Scissors') {
+        return "It's a DRAW"
+      } else if (computerSelection === 'Rock') {
+        computer++
+        return 'You LOSE! Rock beats Scissors'
+      } else {
+        player++
+        return 'You WIN! Scissors beats Paper'
+      }
+  }
+}
+
